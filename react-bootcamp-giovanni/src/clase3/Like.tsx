8e131d3820cd693f375930af5db3ceafddc74986
Giovanni La Volpe <giovanni.lavolpe@gm2dev.com>
2026-08-25 11:24:53 -0300

    feat(Like.tsx) add Like component

---
+++ react-bootcamp-giovanni/src/clase3/Like.tsx
@@ -0,0 +1,22 @@
+import { useState } from "react"
+import '../App.css'
+
+function Like() {
+  const [like, setLike] = useState(false)
+
+  return (
+      <section>
+        <button
+          type="button"
+          className={like ? "liked" : "disliked"}
+          onClick={() => setLike((like) => !like)
+          }
+          >
+            <i class="fa-solid fa-thumbs-up"></i>
+            {like ? "Like" : "Dislike"}
+        </button>
+      </section>
+      )
+}
+
+export default Like

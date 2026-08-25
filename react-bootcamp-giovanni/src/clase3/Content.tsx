ea751ca5a62980dd0a987e3bf8b917648129ac73
Giovanni La Volpe <giovanni.lavolpe@gm2dev.com>
2026-08-25 10:25:54 -0300

    feat(Content.tsx)show the counter function

---
+++ react-bootcamp-giovanni/src/clase3/Content.tsx
@@ -0,0 +1,19 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import Counter from "./Counter"
+
+createRoot(document.getElementById('root')!).render(
+    <StrictMode>
+    </StrictMode>
+)
+
+function Content() {
+    
+    return(
+        <div>
+            <Counter/>
+        </div>
+    )
+}
+
+export default Content;

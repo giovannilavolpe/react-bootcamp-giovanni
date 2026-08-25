3fc0e8f1d0f28eda565eada6717fff60a16a9f12
Giovanni La Volpe <giovanni.lavolpe@gm2dev.com>
2026-08-25 10:26:43 -0300

    feat(Counter.tsx) add Counter buttons

---
+++ react-bootcamp-giovanni/src/clase3/Counter.tsx
@@ -0,0 +1,29 @@
+import { useState } from "react"
+
+
+function Counter() {
+  const [count, setCount] = useState(0)
+
+  return (
+      <section>
+        <button
+          type="button"
+          className="counter"
+          onClick={() => setCount((count) => count + 1)}
+          >
+          Pulsa aquí para sumar
+        </button>
+        <span style={{ margin: "10px"}}>{count}</span>
+        <button
+          type="button"
+          className="counter"
+          onClick={() => setCount((count) => count - 1)}
+          >
+          Pulsa aquí para restar
+        </button>
+      </section>
+      )
+}
+
+export default Counter
+

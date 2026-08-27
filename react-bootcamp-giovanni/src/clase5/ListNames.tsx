2c255a487386b66b622dde6aff4567a1ddba587c
Giovanni La Volpe <giovanni.lavolpe@gm2dev.com>
2026-08-27 09:59:43 -0300

    feat(ListNames.tsx) add listnames function

---
+++ react-bootcamp-giovanni/src/clase5/ListNames.tsx
@@ -0,0 +1,27 @@
+import { useState } from "react"
+
+function ListNames () {
+
+    const NAMES = [
+        {id: 1, name: "Tobias"},
+        {id: 2, name: "Gonza"}, 
+        {id: 3, name: "Jeremias"},
+        {id: 4, name: "Barua"},
+        {id: 5, name: "Giovanni"},]
+
+        const [names, useNames] = useState(NAMES)
+
+    return (
+        <ul style={{ listStyle: 'none'}}>
+            {names.map((item) =>
+            <li key={item.id} style={{ listStyle: 'none', backgroundColor: '#E0B0FF', borderRadius: '20px', color: 'black', width: '20%' }}>
+                <p style={{ marginTop: '26px' }}>ID: {item.id}</p>
+                <p>Name: {item.name}</p>
+            </li>
+            )}
+        </ul>
+    )
+
+}
+
+export default ListNames

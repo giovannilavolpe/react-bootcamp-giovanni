069f8eccc811fcd59e5aee491f3b09b17eeff9d9
Giovanni La Volpe <giovanni.lavolpe@gm2dev.com>
2026-08-28 09:44:45 -0300

    feat(Content.tsx) show counter function

---
+++ react-bootcamp-giovanni/src/clase7/Content.tsx
@@ -0,0 +1,14 @@
+import '../App.css'
+import Counter2 from './Counter2';
+
+
+function Content() {
+    
+    return(
+        <div>
+            <Counter2/>
+        </div>
+    )
+}
+
+export default Content;

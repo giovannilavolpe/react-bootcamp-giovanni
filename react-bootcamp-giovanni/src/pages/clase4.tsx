f09268ff36fca81204bab7c12ce564da41ae132b
Giovanni La Volpe <giovanni.lavolpe@gm2dev.com>
2026-09-01 10:36:15 -0300

    feat(pages) add page with all exercises

---
+++ react-bootcamp-giovanni/src/pages/clase4.tsx
@@ -0,0 +1,15 @@
+import Content from "../clase4/Content"
+
+
+
+function Clase4Page(){
+
+
+    return (
+        <div>
+            <Content/>
+        </div>
+    )
+}
+
+export default Clase4Page
---
+++ react-bootcamp-giovanni/src/pages/clase9.tsx
@@ -0,0 +1,15 @@
+import Content from "../clase9/Content"
+
+
+
+function Clase9Page(){
+
+
+    return (
+        <div>
+            <Content/>
+        </div>
+    )
+}
+
+export default Clase9Page

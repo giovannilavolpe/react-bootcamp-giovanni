9be5e6463524db3b7f37a1986ce8dd4367a6a71a
Giovanni La Volpe <giovanni.lavolpe@gm2dev.com>
2026-09-02 10:59:41 -0300

    feat(Content) show content of tasklist, counter and useLocalStorage

--- react-bootcamp-giovanni/src/clase10/Content.tsx
+++ react-bootcamp-giovanni/src/clase10/Content.tsx
@@ -1,15 +1,23 @@
 import '../App.css'
+import Counter from '../clase10/Counter';
 import InfoTask from './TaskList';
-
+import { ThemeProvider } from './UseLocalStorage';
+import ThemeButton from './UseLocalStorageContext';
 
 function Content() {
     
     return(
         <div>
-            <InfoTask texto="Hacer el ejercicio" priority="alta"/>
-            <InfoTask texto="Hacer commit" priority="alta"/>
-            <InfoTask texto="Pushear" priority="alta"/>
-            <InfoTask texto="Ver tiktok" priority="baja"/>
+            <ThemeProvider>
+                <ThemeButton>
+                    <InfoTask texto="Hacer el ejercicio" priority="alta"/>
+                    <InfoTask texto="Hacer commit" priority="alta"/>
+                    <InfoTask texto="Pushear" priority="alta"/>
+                    <InfoTask texto="Ver tiktok" priority="baja"/>
+                    <Counter initialValue={1000}/>
+                </ThemeButton>
+            </ThemeProvider>
+                
         </div>
     )
 }
